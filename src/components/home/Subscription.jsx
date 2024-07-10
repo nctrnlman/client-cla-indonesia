e8ff42616ca89df1da31162472487b13079cdd1d
Rhazes Devino <rhazesd@gmail.com>
2024-07-10 22:00:29 +0800

    feat(legal associate)

--- src/components/home/Subscription.jsx
+++ src/components/home/Subscription.jsx
@@ -50,11 +50,17 @@ function Subscription() {
   ];
   return (
     <div className="flex flex-col justify-center items-center pt-20">
-      <div>
-        <h2 className="mb-6 text-center text-5xl tracking-tight font-extrabold text-primary">
+      <div className="flex flex-col justify-center items-center ">
+        <button
+          className="bg-secondary items-center p-4 px-6 rounded-full text-primary font-extrabold text-lg hover:bg-primary hover:text-secondary transition duration-300
+          "
+        >
+          PRODUCT
+        </button>
+        <h2 className="mb-6 mt-5 text-center text-5xl tracking-tight font-extrabold text-primary">
           3 Months Subscription
         </h2>
-        <p className="mb-5  pb-12 font-light text-black sm:text-2xl text-center">
+        <p className="mb-5  pb-12 font-semibold text-black sm:text-2xl text-center">
           for Legal Associate
         </p>
       </div>
@@ -71,13 +77,38 @@ function Subscription() {
           />
         ))}
       </div>
-      <div className="flex justify-start flex-col w-full md:w-auto text-base p-4">
-        <span className="mb-2">Notes:</span>
-        <span className="text-sm md:text-base">
-          Payment will be processed monthly for a duration of 3 consecutive
-          months. Price does not include Taxes, Transportation Costs, and other
-          fees beyond the Services mentioned above (if any).
-        </span>
+
+      <div className="flex flex-col justify-center gap-16 items-center mx-auto h-full max-w-7xl pb-20">
+        {/* note */}
+        <div className="bg-[#3B7EA1] text-white flex p-6 rounded-lg gap-4">
+          {/* icon */}
+          <div>
+            <svg
+              width="47"
+              height="47"
+              viewBox="0 0 47 47"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clipPath="url(#clip0_412_677)">
+                <path
+                  d="M23.5 3.91699C12.69 3.91699 3.91669 12.6903 3.91669 23.5003C3.91669 34.3103 12.69 43.0837 23.5 43.0837C34.31 43.0837 43.0834 34.3103 43.0834 23.5003C43.0834 12.6903 34.31 3.91699 23.5 3.91699ZM23.5 33.292C22.4229 33.292 21.5417 32.4107 21.5417 31.3337V23.5003C21.5417 22.4232 22.4229 21.542 23.5 21.542C24.5771 21.542 25.4584 22.4232 25.4584 23.5003V31.3337C25.4584 32.4107 24.5771 33.292 23.5 33.292ZM25.4584 17.6253H21.5417V13.7087H25.4584V17.6253Z"
+                  fill="#F4F5F8"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_412_677">
+                  <rect width="47" height="47" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+          <div className="text-lg">
+            Payment will be processed monthly for a duration of 3 consecutive
+            months. Price does not include Taxes, Transportation Costs, and
+            other fees beyond the Services mentioned above (if any).
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/legal-associate/Hero.jsx
+++ src/components/legal-associate/Hero.jsx
@@ -1,20 +1,65 @@
 function Hero() {
   return (
-    <div className="bg-[#E5E5E5] h-screen">
-      <div className="flex flex-col justify-center gap-16 items-center h-full">
-        <p className="text-center text-[#003262] font-bold text-xl sm:text-2xl md:text-4xl lg:text-5xl px-4 sm:px-8">
+    <div className="h-screen">
+      <div className="flex flex-col justify-center gap-4 items-center mx-auto h-full max-w-7xl">
+        <button
+          className="bg-secondary p-4 px-6 rounded-full text-primary font-extrabold text-lg hover:bg-primary hover:text-secondary transition duration-300
+          "
+        >
+          ABOUT
+        </button>
+        <p className="text-center text-[#003262] font-extrabold outline-2 text-xl sm:text-2xl md:text-4xl lg:text-5xl px-4 sm:px-8 ">
           LEGAL ASSOCIATE
         </p>
-        <div className="flex gap-8">
-          <button
-            className="bg-secondary p-4 px-6 rounded-full text-primary font-semibold hover:bg-primary hover:text-secondary transition duration-300
-          "
-          >
-            Get Started
-          </button>
-          <button className="bg-secondary p-4 px-6 rounded-full text-primary font-semibold hover:bg-primary hover:text-secondary transition duration-300">
-            Order Now
-          </button>
+        {/* desc */}
+        <div className="bg-gradient-to-b from-[#003262] to-[#3B7EA1] h-96 border rounded-xl p-4 mt-14">
+          {/* icon */}
+          <div className="text-center items-center flex justify-center pb-4">
+            <svg
+              width="113"
+              height="113"
+              viewBox="0 0 113 113"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.7125 84.3301H100.727V93.7001H11.7125V84.3301Z"
+                fill="white"
+              />
+              <path
+                d="M103.07 98.3848H9.37C8.12746 98.3848 6.93581 98.8784 6.0572 99.757C5.17859 100.636 4.685 101.827 4.685 103.07V107.755H107.755V103.07C107.755 101.827 107.261 100.636 106.383 99.757C105.504 98.8784 104.313 98.3848 103.07 98.3848Z"
+                fill="white"
+              />
+              <path
+                d="M23.425 42.165H32.795V79.645H23.425V42.165Z"
+                fill="white"
+              />
+              <path
+                d="M51.535 42.165H60.905V79.645H51.535V42.165Z"
+                fill="white"
+              />
+              <path
+                d="M79.645 42.165H89.015V79.645H79.645V42.165Z"
+                fill="white"
+              />
+              <path
+                d="M105.178 26.7041L56.22 4.68457L7.26175 26.7041C6.49848 27.1171 5.85806 27.7248 5.40551 28.4653C4.95296 29.2058 4.70438 30.0529 4.685 30.9206V32.7946C4.685 34.0371 5.17859 35.2288 6.0572 36.1074C6.93581 36.986 8.12746 37.4796 9.37 37.4796H103.07C104.313 37.4796 105.504 36.986 106.383 36.1074C107.261 35.2288 107.755 34.0371 107.755 32.7946V30.9206C107.736 30.0529 107.487 29.2058 107.034 28.4653C106.582 27.7248 105.942 27.1171 105.178 26.7041Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+
+          {/* text */}
+          <div>
+            <p className=" text-justify text-2xl p-4 text-white ">
+              Elevate your business&apos;s legal journey with our Legal
+              Associate as your trusted partner. We provide legal assistance
+              tailored to your company&apos;s needs and type. Our innovative
+              solutions, quick responses, and flexible services ensure that your
+              legal needs are met efficiently. Partner with us to unlock your
+              business&apos;s full potential for growth and success.
+            </p>
+          </div>
         </div>
       </div>
     </div>
