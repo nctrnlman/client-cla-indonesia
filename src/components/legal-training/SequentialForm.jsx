2b8a8d047a6d51580a4640ae47d43a3f1b5a0400
Uzlah <landakuz@gmail.com>
2024-07-12 13:37:17 +0700

    Change Logo

--- src/components/legal-training/SequentialForm.jsx
+++ src/components/legal-training/SequentialForm.jsx
@@ -1,43 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import LoadingSpinner from "../Loading/LoadingSpinner";
 
 function ContactUsSection() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneCode, setPhoneCode] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneCode: "",
+    phoneNumber: "",
+    message: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const formRef = useRef(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
+
+    const mailtoLink = `mailto:uzlahfirmansyah@gmail.com?subject=Contact Form Submission&body=Name: ${formData.name}%0D%0AEmail: ${formData.email}%0D%0APhone: ${formData.phoneCode}${formData.phoneNumber}%0D%0AMessage: ${formData.message}`;
+    window.open(mailtoLink, "_blank");
+
     await new Promise((resolve) => setTimeout(resolve, 2000));
     setIsLoading(false);
     setShowAlert(true);
     setTimeout(() => {
       setShowAlert(false);
-      setName("");
-      setEmail("");
-      setPhoneCode("");
-      setPhoneNumber("");
-      setMessage("");
+      setFormData({
+        name: "",
+        email: "",
+        phoneCode: "",
+        phoneNumber: "",
+        message: "",
+      });
     }, 3000);
   };
 
-  const renderField = (
-    value,
-    setValue,
-    type = "text",
-    placeholder,
-    showIf = true
-  ) => (
+  useEffect(() => {
+    if (formRef.current) {
+      formRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [formData.email, formData.phoneNumber]);
+
+  const renderField = (name, type = "text", placeholder, showIf = true) => (
     <div
       className={`transition-all duration-500 ease-in-out w-full ${
-        showIf
-          ? "max-h-40 opacity-100 mb-4"
-          : "max-h-0 opacity-0 overflow-hidden"
+        showIf ? "max-h-40 opacity-100 mb-4" : "max-h-0 opacity-0 overflow-hidden"
       }`}
     >
       <label className="form-control w-full">
@@ -46,10 +58,11 @@ function ContactUsSection() {
         </div>
         <input
           type={type}
+          name={name}
           placeholder={placeholder}
           className="input input-bordered w-full"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          value={formData[name]}
+          onChange={handleChange}
         />
       </label>
     </div>
@@ -58,9 +71,7 @@ function ContactUsSection() {
   const renderPhoneField = (showIf = true) => (
     <div
       className={`transition-all duration-500 ease-in-out w-full ${
-        showIf
-          ? "max-h-40 opacity-100 mb-4"
-          : "max-h-0 opacity-0 overflow-hidden"
+        showIf ? "max-h-40 opacity-100 mb-4" : "max-h-0 opacity-0 overflow-hidden"
       }`}
     >
       <label className="form-control w-full">
@@ -70,8 +81,9 @@ function ContactUsSection() {
         <div className="flex">
           <select
             className="select select-bordered w-1/3 mr-2"
-            value={phoneCode}
-            onChange={(e) => setPhoneCode(e.target.value)}
+            name="phoneCode"
+            value={formData.phoneCode}
+            onChange={handleChange}
           >
             <option value="" disabled>
               Code
@@ -80,10 +92,11 @@ function ContactUsSection() {
           </select>
           <input
             type="tel"
+            name="phoneNumber"
             placeholder="Phone number"
             className="input input-bordered w-2/3"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            value={formData.phoneNumber}
+            onChange={handleChange}
           />
         </div>
       </label>
@@ -92,41 +105,32 @@ function ContactUsSection() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-darkBlue pt-20 px-4 relative">
-      <div className="flex w-full max-w-6xl">
-        <div className="card bg-base-300 rounded-box grid flex-grow place-items-center p-8">
+      <div className="w-full max-w-6xl flex flex-col lg:flex-row">
+        {/* Section hanya terlihat di desktop */}
+        <div className="hidden lg:flex card bg-transparent rounded-box grid flex-grow place-items-center p-8">
           <h1 className="text-5xl font-bold text-primary mb-4">Contact Us</h1>
           <p className="text-xl text-center mb-4">
             Get in touch with us for any inquiries
           </p>
         </div>
-        <div className="divider divider-horizontal text-gray-300">OR</div>
-        <div className="card bg-base-300 rounded-box grid flex-grow place-items-center p-8">
+
+        {/* Formulir kontak */}
+        <div className="card bg-transparent rounded-box grid flex-grow place-items-center p-8">
           <div className="mockup-phone border-primary shadow-lg shadow-blue-500/50">
-            {" "}
-            {/* Added shadow for underglow */}
             <div className="camera"></div>
             <div className="display">
               <div className="artboard artboard-demo phone-1 overflow-y-auto bg-white">
-                <div className="p-4">
+                <div className="p-4" ref={formRef}>
                   <h2 className="text-2xl font-bold text-center mb-4 text-primary">
                     Contact Form
                   </h2>
                   <form onSubmit={handleSubmit} className="space-y-4">
-                    {renderField(name, setName, "text", "What is your name?")}
-                    {renderField(
-                      email,
-                      setEmail,
-                      "email",
-                      "What is your email?",
-                      name !== ""
-                    )}
-                    {renderPhoneField(email !== "")}
-
+                    {renderField("name", "text", "What is your name?")}
+                    {renderField("email", "email", "What is your email?", formData.name !== "")}
+                    {renderPhoneField(formData.email !== "")}
                     <div
                       className={`transition-all duration-500 ease-in-out w-full ${
-                        phoneNumber !== ""
-                          ? "max-h-60 opacity-100 mb-4"
-                          : "max-h-0 opacity-0 overflow-hidden"
+                        formData.phoneNumber !== "" ? "max-h-60 opacity-100 mb-4" : "max-h-0 opacity-0 overflow-hidden"
                       }`}
                     >
                       <label className="form-control w-full">
@@ -136,18 +140,15 @@ function ContactUsSection() {
                         <textarea
                           className="textarea textarea-bordered h-24 w-full"
                           placeholder="Type your message here"
-                          value={message}
-                          onChange={(e) => setMessage(e.target.value)}
+                          name="message"
+                          value={formData.message}
+                          onChange={handleChange}
                         ></textarea>
                       </label>
                     </div>
-
-                    {message && (
+                    {formData.message && (
                       <div className="flex justify-center">
-                        <button
-                          type="submit"
-                          className="btn btn-primary w-full"
-                        >
+                        <button type="submit" className="btn btn-primary w-full">
                           Send
                         </button>
                       </div>
@@ -163,11 +164,11 @@ function ContactUsSection() {
       {isLoading && <LoadingSpinner />}
 
       {showAlert && (
-        <div className="fixed top-4 right-4 z-50">
-          <div role="alert" className="alert alert-success">
+        <div className="fixed top-4 right-4 z-50 animate-slideIn">
+          <div role="alert" className="alert alert-success shadow-lg">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
+              className="stroke-current shrink-0 h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
             >
