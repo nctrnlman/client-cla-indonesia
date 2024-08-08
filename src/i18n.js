441fc6bb4f3cf00c114c7a73e4367cadd6ac9d50
Rhazes Devino <rhazesd@gmail.com>
2024-08-09 01:27:05 +0800

    adjust translate subscription

--- src/i18n.js
+++ src/i18n.js
@@ -6,18 +6,21 @@ import translationEN from "./locales/en/translation.json";
 import navbarEN from "./locales/en/navbar.json";
 import homeEN from "./locales/en/home.json";
 import faqEN from "./locales/en/faq.json";
+import subscriptionEN from "./locales/en/subscription.json";
 import contactUsEN from "./locales/en/contactUs.json";
 import serviceDataEN from "./locales/en/serviceData.json";
 import translationID from "./locales/id/translation.json";
 import navbarID from "./locales/id/navbar.json";
 import homeID from "./locales/id/home.json";
 import faqID from "./locales/id/faq.json";
+import subscriptionID from "./locales/id/subscription.json";
 import contactUsID from "./locales/id/contactUs.json";
 import serviceDataID from "./locales/id/serviceData.json";
 // import translationZH from "./locales/zh/translation.json";
 import navbarZH from "./locales/zh/navbar.json";
 import homeZH from "./locales/zh/home.json";
 import faqZH from "./locales/zh/faq.json";
+import subscriptionZH from "./locales/zh/subscription.json";
 import contactUsZH from "./locales/zh/contactUs.json";
 import serviceDataZH from "./locales/zh/serviceData.json";
 
@@ -27,6 +30,7 @@ const resources = {
     navbar: navbarEN,
     home: homeEN,
     faq: faqEN,
+    subscription: subscriptionEN,
     serviceData: serviceDataEN,
     contactUs: contactUsEN,
   },
@@ -35,6 +39,7 @@ const resources = {
     navbar: navbarID,
     home: homeID,
     faq: faqID,
+    subscription: subscriptionID,
     serviceData: serviceDataID,
     contactUs: contactUsID,
   },
@@ -43,6 +48,7 @@ const resources = {
     navbar: navbarZH,
     home: homeZH,
     faq: faqZH,
+    subscription: subscriptionZH,
     serviceData: serviceDataZH,
     contactUs: contactUsZH,
   },
