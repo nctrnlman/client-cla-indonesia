2d7919e121db1b0ea71d214e257d476c999256ef
Rhazes Devino <rhazesd@gmail.com>
2024-08-11 06:17:37 +0800

    fix other service detail

--- src/components/other-service-detail/Breadcrumb.jsx
+++ src/components/other-service-detail/Breadcrumb.jsx
@@ -1,12 +1,12 @@
 function Breadcrumb({ title }) {
   return (
-    <div>
+    <div className="py-4 ">
       <nav className="flex" aria-label="Breadcrumb">
         <ol className="inline-flex flex-wrap items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
             <a
               href="/"
-              className="inline-flex items-center text-sm font-medium text-secondary "
+              className="inline-flex items-center text-sm font-medium text-secondary"
             >
               <svg
                 className="w-3 h-3 me-2.5"
@@ -17,7 +17,7 @@ function Breadcrumb({ title }) {
               >
                 <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
               </svg>
-              Other Service
+              CLA
             </a>
           </li>
           <li>
@@ -39,7 +39,7 @@ function Breadcrumb({ title }) {
               </svg>
               <a
                 href="#"
-                className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+                className="text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
               >
                 {title}
               </a>
