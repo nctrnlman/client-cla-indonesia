fc196d97d275e8a59d56b01a1f60f0290308c8f1
Rhazes Devino <rhazesd@gmail.com>
2024-09-04 01:16:48 +0700

    fixing uat navbar

--- src/utils/whatsappUtils.js
+++ src/utils/whatsappUtils.js
@@ -1,5 +1,5 @@
 export const generateWhatsAppUrl = (serviceTitle) => {
-  const phoneNumber = "6281221431716";
+  const phoneNumber = "6281211110564";
   const message = serviceTitle
     ? `Hai, saya tertarik dengan paket ${serviceTitle}. Bisakah Anda memberikan detail lebih lanjut?`
     : "Hai, saya tertarik dengan layanan Anda.";
