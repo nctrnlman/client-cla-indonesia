4934e1ee6e393ec84a2dec1156fbb2011e5ae3f8
Khaled Makkawirelang <makkawirelang@gmail.com>
2024-09-03 13:54:13 +0700

    revision from page 2 - 5

--- src/components/home/LegalityPackages.jsx
+++ src/components/home/LegalityPackages.jsx
@@ -30,7 +30,7 @@ function LegalityPackages() {
           <h2 className="text-4xl md:text-5xl font-extrabold text-primary mb-4">
             {t("legality_packages")}
           </h2>
-          <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+          <p className="text-xl text-gray-800 max-w-3xl mx-auto">
             {t("choose_package")}
           </p>
         </motion.div>
